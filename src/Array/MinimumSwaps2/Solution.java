450a5f9952ae14583f45aeb397cfa56c1bc90e19
Bilyana Vatev <engineer_bilyana_vatev@email.com>
2021-04-17 08:50:46 +0100

    problem

---
+++ src/Array/MinimumSwaps2/Solution.java
@@ -0,0 +1,50 @@
+package Array.MinimumSwaps2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    static int minimumSwaps(int[] arr) {
+       int i = 0, count = 0, n = arr.length, temp;
+
+       while (i < n){
+           if(arr[i] != i + 1){
+               temp = arr[i];
+               arr[i] = arr[temp - 1];
+               arr[temp - 1] = temp;
+               count ++;
+           } else {
+               i ++;
+           }
+       }
+       return count;
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(scanner.nextLine());
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        scanner.close();
+
+        int res = minimumSwaps(arr);
+        System.out.println(res);
+    }
+}
